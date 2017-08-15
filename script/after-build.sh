1739f51f7fbc93c8cc79473efa6fb2e01ea637be
maxincai <maxincai@126.com>
2017-08-15 19:36:54 +0800

    增加nginx配置和编译脚本

---
+++ script/after-build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#-------------------------------------------------------------------------
+# Filename: build.sh
+# Author: 马新才
+# Date: 2017-08-15
+# Description: 本脚本的作用是在build代码
+#  -------------------------------------------------------------------------
+
+app_path='/data/www/project-taochemao-wap'
+
+# 删除gitignore文件方便将编译后的文件加入到build git
+delete_gitignore()
+{
+    if [ -f "${app_path}/dist/.gitignore" ]
+    then
+        rm -rf ${app_path}/dist/.gitignore
+    fi
+}
+
+main()
+{
+    delete_gitignore
+}
+
+main
