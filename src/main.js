24911b1eec4d4da3e0215e28a75be57dc410fa97
huzaixing123456 <252174621@qq.com>
2017-08-22 18:50:33 +0800

    全局错误提醒

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-// The Vue build version to load with the `import` command
+git // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
@@ -17,6 +17,7 @@ Validator.addLocale(zh_CN);
 Vue.use(VeeValidate, {
 	locale:'zh_CN'
 });
+
 
 
 
