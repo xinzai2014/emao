e04045d0997c26a67ec0a8c91059cc22c6e02a6a
huzaixing123456 <252174621@qq.com>
2017-08-31 10:21:57 +0800

    淘车猫BUG修复

--- src/page/index/serie.vue
+++ src/page/index/serie.vue
@@ -8,7 +8,7 @@
                 <p class="index-car-name">{{item.name}}</p>
                 <p class="index-car-price"><span>{{item.minPrice}}</span>万起</p>
                 <p class="index-car-count">共<i>{{item.saleCars}}</i>个车型在售</p>
-                <p class="index-car-sale">最高下 <strong>{{item.maxFall}}万</strong></p>
+                <p class="index-car-sale" v-if="item.maxFall>0">最高下 <strong>{{item.maxFall}}万</strong></p>
             </li>
         </ul>
         <p class="index-more-brand" v-show="serieMore" @click="getSerie">查看更多 <i class="yellow-bt"></i></p>
