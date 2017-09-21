6c34e53097a777e01a323fd8b728ddf3d4656d3f
huzaixing123456 <252174621@qq.com>
2017-09-21 14:40:22 +0800

    上传组件更新

--- src/components/common/uploader/uploader.vue
+++ src/components/common/uploader/uploader.vue
@@ -210,26 +210,28 @@
             },
             closeImg(){ 
                 this.showBigImg = false;
+            },
+            init(){
+                this.initData = this.uploadData;
+                if("imgArr" in this.uploadData){
+                    var img = this.uploadData.imgArr;
+                    if(img.length == 0) return;
+                    this.files = [];
+                    var that = this;
+                    img.forEach(function(value,index){
+                        var obj = {};
+                        obj.src = value;
+                        that.files.push(obj);
+                    })
+                }
             }
         },
         mounted(){
-            this.initData = this.uploadData;
-            if("imgArr" in this.uploadData){
-                var img = this.uploadData.imgArr;
-                if(img.length == 0) return;
-                this.files = [];
-                var that = this;
-                img.forEach(function(value,index){
-                    var obj = {};
-                    obj.src = value;
-                    that.files.push(obj);
-                })
-            }
-            
+            this.init();
         },
         watch:{
             uploadData:function(){
-                console.log("有变动了");
+                this.init();
             }
         }
     }
