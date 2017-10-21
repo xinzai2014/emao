924edb34e5d831bc99a7055f58a2cd70b6913dc8
lilianghong <18610605671@163.com>
2017-10-21 20:12:29 +0800

    消息列表页，只能加载10条数据调试终结版

--- src/page/message/children/order.vue
+++ src/page/message/children/order.vue
@@ -43,7 +43,8 @@
 	            nowPage : 1, //第几页
 	            lastPage : 0,
 	            switchShow :false, //加载更多
-	            loadingData : false
+	            loadingData : false,
+				scrollTimer: null
                 
             }
         },
@@ -102,10 +103,15 @@
 			scroll: {
 				bind: function (el, binding){
 					window.addEventListener('scroll', function () {
-						if(document.body.scrollTop + window.innerHeight >= el.clientHeight) {
-							var fnc = binding.value; 
-							fnc(); 
-						}
+
+						clearTimeout(this.scrollTimer);
+						this.scrollTimer = setTimeout(function(){
+							var scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
+							if(scrollTop + window.innerHeight >= el.clientHeight ) {
+								var fnc = binding.value;
+								fnc();
+							}
+						}, 1000);
 					})
 				}
 			}
