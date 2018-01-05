2dac2b1bd47716033d67a508225c471760001545
jifangli <jifangli116@emao.com>
2018-01-05 20:10:39 +0800

    ff

--- src/page/empower/empower.vue
+++ src/page/empower/empower.vue
@@ -661,14 +661,14 @@
             telephoneButton(){ //电话
             	var obj = {
 	        		actionname:"telephoneButton",//Native 函数名称：必填，Native 提供给 JS 的可用函数的函数名称
-	        		params:{hidden:this.showPhone}//hidden=0显示电话按钮，hidden=1隐藏电话按钮
+	        		params:{hidden:0}//hidden=0显示电话按钮，hidden=1隐藏电话按钮
 	    		};
 	            this.tcmApp(obj);//tcmApp 函数参见通信规则中的示例说明
             },
             enableGobackButton(){ //禁止返回
             	var obj = {
 	      			actionname:"enableGobackButton",//Native 函数名称：必填，Native 提供给 JS 的可用函数的函数名称
-	        		params:{enable:showEnable, title: "确定退出授权店认证"}//enable=0不允许返回，enable=1允许返回
+	        		params:{enable:0, title: "确定退出授权店认证"}//enable=0不允许返回，enable=1允许返回
 	    		};
 	            this.tcmApp(obj);//tcmApp 函数参见通信规则中的示例说明
             },
