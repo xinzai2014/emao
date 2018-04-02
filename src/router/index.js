c4bf5fff4a94cc2e559209c98201a44692431003
季芳莉 <jifangli116@emao.com>
2018-04-02 20:04:14 +0800

    token

--- src/router/index.js
+++ src/router/index.js
@@ -517,7 +517,7 @@ router.beforeEach((to, from, next) => {
     if(token == null){ //用于app内部跳转多个参数后续完善
         var href = window.location.href,
             str = href.indexOf('token=');
-            alert(window.location.href);
+            alert(this.$route.query.token);
             if(str != -1){
                 token = href.substr(str+6);
             }  
