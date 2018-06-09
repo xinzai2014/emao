a7e96ef1e176367de18afdaa62d3bf1f278d8143
lilianghong <18610605671@163.com>
2018-06-09 23:47:10 +0800

    ios倒计时

--- src/common/js/countdown.js
+++ src/common/js/countdown.js
@@ -33,8 +33,10 @@ class Countdown {
       return;
     }
     let returnContent = [];
-    const state = (nowTime > startTime && nowTime <= endTime) ? 'ing' : nowTime <= startTime ? 'start' : 'end';
-    alert(state)
+    const state = (nowTime > startTime && nowTime <= startTime) ? 'ing' : nowTime <= startTime ? 'start' : 'end';
+    alert(nowTime+'当前')
+    alert(startTime+'开始')
+    alert(startTime+'结束')
     returnContent.push(state);
     if (state === 'end') {
       if (type === 'double') {
