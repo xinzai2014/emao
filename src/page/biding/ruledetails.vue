fb535d8a6912599787318eaa57a671e51d5d922a
cuizhiwei <cuizhiwei1310@emao.com>
2018-07-27 18:12:15 +0800

    保证金细则修改

--- src/page/biding/ruledetails.vue
+++ src/page/biding/ruledetails.vue
@@ -9,13 +9,12 @@
             <p class="item_title">2.保证金的返还</p>
             <div class="item_cont">在符合以下条件时将原路退还用户保证金：</div>
             <div class="item_cont">1.用户未成功竞拍商品：例如在竞拍过程中始终未出价、在竞拍过程中放弃加价等情况；</div>
-            <div class="item_cont">2.竞拍成功，确认提车后系统会在1-8个工作日内将款项退还到支付账号中；</div>
-            <div class="item_cont">3.未获拍，竞拍结束后系统会自动在1-8个工作日内将款项退还到支付账号中，如您缴纳的竞拍保证金符合退还条件，但是系统未予退还，您可以联系客服<span>400-825-2368</span>我们会尽快帮您处理。</div>
+            <div class="item_cont">2.竞拍成功，确认提车后系统会在1-7个工作日内将款项退还到支付账号中；</div>
+            <div class="item_cont">3.未获拍，竞拍结束后系统会自动在1-7个工作日内将款项退还到支付账号中，如您缴纳的竞拍保证金符合退还条件，但是系统未予退还，您可以联系客服<span>400-825-2368</span>我们会尽快帮您处理。</div>
         </div>
         <div class="list_item">
             <p class="item_title">3.保证金的扣除</p>
             <div class="item_cont">1.在竞拍成功之时起的3天内未按照成交价格支付商品货款的；</div>
-            <div class="item_cont">2.在竞拍成功且提交汇款凭证后，竞拍人申请退款。</div>
         </div>
     </div>
 </template>
