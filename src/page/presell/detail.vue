c30feffbc135dd9f141705c60aa3407ebd75b3a9
houluyao9001-pq@emao.com <houluyao9001-pq@emao.com>
2018-07-30 22:39:01 +0800

    添加活动状态已售罄

--- src/page/presell/detail.vue
+++ src/page/presell/detail.vue
@@ -113,7 +113,7 @@
             <section class="car-reserve-btn">
                 <h4 class="soldOut-warning" v-show="countdownText === '已售罄'">该商品已抢光，敬请期待下次抢购！</h4>
                 <div class="bottom-btn-box">
-                    <div class="bottom-btn bottom-btn-left" v-show="isTcmApp" >
+                    <div class="bottom-btn bottom-btn-left" v-show="isTcmApp" v-if="btnState">
                         <div class="car-presell-present-disabled color" @click="gofinancing">融资购车</div>
                     </div>
                     <div class="bottom-btn bottom-btn-right">
