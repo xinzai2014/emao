137093711092c301fc0ae24a0e2304824db4bf7f
cuizhiwei <cuizhiwei1310@emao.com>
2018-07-30 15:50:52 +0800

    打印内容注销

--- src/page/biding/biding.vue
+++ src/page/biding/biding.vue
@@ -287,7 +287,7 @@ export default {
            this.addShareButton();
           })
           .catch(error => {
-            console.log(error,"app获取数据错")
+            
             this.tost(error.body.msg);
           });
       } else {
@@ -311,14 +311,14 @@ export default {
 
           })
           .catch(error => {
-            console.log(error,"wap获取数据错")
+         
           });
       }
     },
     //data赋值
     assignment(data){
            if (data) {
-        console.log(data)
+        
         this.sowingMap = data.sowingMap; //轮播图
         if(this.sowingMap.length){
           for (var i = 0; i < this.sowingMap.length; i++) {
@@ -454,7 +454,7 @@ export default {
       .then((res)=>{
         if(this.bidderStatus!==4){
         let data = res.body
-        console.log("广播数据",res)
+        
         this.enrolment=data.enrolment; //报名人数
         this.settingRemind=data.settingRemind;//设置闹钟人数
         
@@ -478,7 +478,7 @@ export default {
          }
       })
       .catch((e)=>{
-        // console.log(e,"获取数据错")
+       
         if(this.bidderStatus=="4"){
           this.endnum++;
           if(this.endnum==3){
@@ -658,7 +658,7 @@ export default {
       if (!this.isBtnDisable) {
         if (this.bottomBtnText === "交保证金报名") {
           this.setBtnClickLog(1);
-          console.log("交保证金报名")
+          
           
           // 跳转到app交保证金页面
           /* openurl = emaotaochemao://push/ensureBidder&bidderId=xxx&bidderMoney=xxx 参数说明:bidderId: 竞拍id  bidderMoney: 竞拍保证金 */
@@ -668,19 +668,19 @@ export default {
         }
         if (this.bottomBtnText === "我要出价") {
           this.setBtnClickLog(2);
-          console.log("我要出价");
+          
           this.popupState = true;
         }
       }
     },
     //加价
     addPrice() {
-      console.log("加价");
+     
       this.myAddPrice = (this.myAddPrice - 0) + (this.increasePrice - 0);
     },
     //减价
     subtractPrice() {
-      console.log("减价");
+      
       if (this.myAddPrice > this.increasePrice) {
         this.myAddPrice = (this.myAddPrice - 0) - (this.increasePrice - 0);
       } else {
@@ -691,7 +691,7 @@ export default {
     bidingHandle() {
       let _this=this;
       this.setBtnClickLog(3);
-      // console.log(this.myAddPrice);
+      
       let params = {
         token: this.$route.query.token,
         bidderId: this.$route.query.bidderId,
@@ -704,7 +704,7 @@ export default {
         params: params
       })
         .then(function(res) {
-           console.log("成功",res); 
+           
           this.tost(res.body.data.msg)
           this.currentPrice=res.body.data.currentPrice
           this.popupState = false;
@@ -712,7 +712,7 @@ export default {
 
         })
         .catch(error => {
-          console.log(error,"出价错")
+          
           this.bidingErrorText = error.body.msg
           this.popupState = false;
           this.popupStatePrice = true;  
@@ -744,7 +744,7 @@ export default {
             this.clockText = "取消提醒";
           })
           .catch(error => {
-            console.log(error,"设置提醒错")
+           
             this.tost("设置失败");
           });
       } else {
@@ -766,7 +766,7 @@ export default {
             this.clockText = "设置提醒";
           })
           .catch(error => {
-            console.log(error,"取消提醒错")
+           
             this.tost("取消失败");
           });
       }
@@ -775,7 +775,7 @@ export default {
     setClockUI() {
       // 判断是否设置提醒 1-未设置 2-已设置
       if (this.remindStatus === "1") {
-        console.log("1-未设置");
+       
         //1.竞拍未开始状态
         if (this.bidderStatus === "1" || this.bidderStatus === "2") {
           this.clockText = "设置提醒";
@@ -787,7 +787,7 @@ export default {
         return false;
       }
       if (this.remindStatus === "2") {
-        console.log("2-已设置");
+       
         //1.竞拍未开始状态
         if (this.bidderStatus === "1" || this.bidderStatus === "2") {
           this.clockText = "取消提醒";
@@ -857,7 +857,7 @@ export default {
       
         })
         .catch(error => {
-          console.log(error,"按钮统计错",buttonType)
+         
         });
     },
 
