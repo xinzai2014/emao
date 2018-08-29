698829ed4d8f219d9cddeea64346729badea7da2
Edianzu <chengkunkun9018-pq@emao.com>
2018-08-29 20:40:36 +0800

    字体大小px转rem'

--- src/page/help/help.vue
+++ src/page/help/help.vue
@@ -3,13 +3,13 @@
         <div class="item">
             <h2>1.如何添加库存?</h2>
             <p>选择要录入车辆的品牌车型。</p>
-            <p>录入车架号。车架号可以通过扫码前挡风的车架号 快速识别录入。</p>
+            <p>录入车架号。车架号可以通过扫码前挡风的车架号快速识别录入。</p>
             <p>保存即可。</p>
             <img src="./images/t1.jpg">
         </div>
         <div class="item">
             <h2>2.如何变更车辆状态?</h2>
-            <p>入口1：在车辆列表中对应每辆车都有快速操作入 口。</p>
+            <p>入口1：在车辆列表中对应每辆车都有快速操作入口。</p>
             <img src="./images/t2.jpg">
             <img src="./images/t3.jpg">
             <p>入口2：在车辆详情下方有对应的操作入口。</p>
@@ -29,31 +29,31 @@ export default {
 </script>
 <style scoped>
     .help {
-        padding-top: 16px;
+        padding-top: .426667rem;
     }
     .item {
         background: #fff;
-        padding: 0 22px;
-        margin-bottom: 16px;
+        padding: 0 .586667rem;
+        margin-bottom: .426667rem;
     }
     .item:last-child {
         margin-bottom: 0;
     }
     .item h2 {
-        padding: 16px 0;
-        font-size: 18px;
+        padding: .426667rem 0;
+        font-size: .48rem;
         font-weight: bold;
     }
     p {
-        font-size: 14px;
+        font-size: .373333rem;
         color: #2c2c2c;
-        margin-bottom: 10px;
-        padding-left: 36px;
-        background: url(images/dot.png) no-repeat 18px 6px;
+        margin-bottom: .266667rem;
+        padding-left: .96rem;
+        background: url(images/dot.png) no-repeat .48rem .16rem;    
     }
     img {
         width: 100%;
-        margin-bottom: 16px;
+        margin-bottom: .426667rem;
     }
 </style>
 
